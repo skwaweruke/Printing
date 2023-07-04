729b97c34adb01fe06044137a2261b538feea045
skwaweru254 <skwaweru254@gmail.com>
2023-07-04 09:28:25 +0300

    Initial commit

---
+++ src/main/java/ke/co/skwaweru254/Main.java
@@ -0,0 +1,9 @@
+package ke.co.skwaweru254;
+
+public class Main {
+    public static void main(String[] args) {
+        Printing obj = new Printing();
+
+        System.out.println(obj.tryPrint());
+    }
+}
---
+++ src/main/java/ke/co/skwaweru254/Printing.java
@@ -0,0 +1,14 @@
+package ke.co.skwaweru254;
+
+public class Printing {
+    public String tryPrint(){
+        System.out.println("Hello world!");
+
+        System.out.printf("%S %s %S %n","Simon Waweru", "is no", "joke");
+        System.out.print("Simon"+"\n");
+        System.out.println("You are the one");
+        System.out.println("Waweru\n");
+        String tryPrint = "It is done";
+        return tryPrint;
+    }
+}
